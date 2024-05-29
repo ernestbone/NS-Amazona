4ff3d4744a4596134a2943a6c561738884575478
ernestbone <owhiroroernest@gmail.com>
2024-05-29 08:53:23 +0100

    first commit

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,30 @@
+import { Row, Col } from 'react-bootstrap'
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
+import ProductItem from '../components/ProductItem'
+import { Helmet } from 'react-helmet-async'
+import { useGetProductsQuery } from '../hooks/productHooks'
+import { ApiError } from '../types/ApiError'
+import { getError } from '../utils'
+
+export default function HomePage() {
+  const { data: products, isLoading, error } = useGetProductsQuery()
+  return isLoading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">
+      {getError(error as unknown as ApiError)}
+    </MessageBox>
+  ) : (
+    <Row>
+      <Helmet>
+        <title>NS Amazona</title>
+      </Helmet>
+      {products!.map((product) => (
+        <Col key={product.slug} sm={6} md={4} lg={3}>
+          <ProductItem product={product} />
+        </Col>
+      ))}
+    </Row>
+  )
+}
